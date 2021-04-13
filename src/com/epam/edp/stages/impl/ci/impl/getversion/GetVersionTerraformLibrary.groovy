9cd92718a586203ca5e3b1b86232bc051b53acf0
Mykola Marusenko <mykola_marusenko@epam.com>
2021-04-13 16:37:31 +0300

    [EPMDEDP-6452]: Implement get-version stage for terraform library
    Change-Id: I3059151c2a9b4966cdfd9719aaba899c872b2b8a

---
+++ src/com/epam/edp/stages/impl/ci/impl/getversion/GetVersionTerraformLibrary.groovy
@@ -0,0 +1,50 @@
+/* Copyright 2019 EPAM Systems.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package com.epam.edp.stages.impl.ci.impl.getversion
+
+
+import com.epam.edp.stages.impl.ci.ProjectType
+import com.epam.edp.stages.impl.ci.Stage
+
+@Stage(name = "get-version", buildTool = "terraform", type = ProjectType.LIBRARY)
+class GetVersionTerraformLibrary {
+    Script script
+
+    def setUpVersions(workDir, codebase, git, job) {
+        if (codebase.config.versioningType == "edp") {
+            updateBuildNumber(codebase.config.name, git.branch.replaceAll(/\//, "-"), codebase.currentBuildNumber)
+            codebase.vcsTag = "build/${codebase.version}"
+            return
+        }
+
+        codebase.version = script.readFile "${workDir}/VERSION"
+        codebase.buildVersion = "${codebase.version}-${script.BUILD_NUMBER}"
+        job.setDisplayName("${script.currentBuild.number}-${git.branch}-${codebase.version}")
+        codebase.vcsTag = "${git.branch}-${codebase.buildVersion}"
+    }
+
+    def updateBuildNumber(codebaseName, branchName, buildNumber) {
+        script.sh """kubectl patch codebasebranches.v2.edp.epam.com ${codebaseName}-${
+            branchName
+        } --type=merge -p '{\"status\": {\"build\": "${buildNumber}"}}'"""
+    }
+
+    void run(context) {
+        setUpVersions(context.workDir, context.codebase, context.git, context.job)
+        context.codebase.deployableModuleDir = "${context.workDir}"
+        script.println("[JENKINS][DEBUG] Application version - ${context.codebase.version}")
+        script.println("[JENKINS][DEBUG] VCS tag - ${context.codebase.vcsTag}")
+    }
+}
