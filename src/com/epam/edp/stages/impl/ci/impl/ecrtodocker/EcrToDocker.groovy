1fe080d5a5752a818d5d7768d519d63e7f41db07
Sergii Masiuk <sergii_masiuk@epam.com>
2022-01-12 15:52:43 +0200

    [EPMDEDP-7974]: refactor: Refactor ecr-to-docker CI stage
    *Change docker hub credentials storage from jenkins to k8s secret
    *Fix image existence check logic
    *Fix errors in uploaded image naming
    
    Change-Id: Ib5e1e24e574484c87b72e290934afbb3f3e59830

--- src/com/epam/edp/stages/impl/ci/impl/ecrtodocker/EcrToDocker.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/ecrtodocker/EcrToDocker.groovy
@@ -1,14 +1,11 @@
-/* Copyright 2021 EPAM Systems.
-
+/* Copyright 2022 EPAM Systems.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
 See the License for the specific language governing permissions and
 limitations under the License.*/
 
@@ -16,43 +13,92 @@ package com.epam.edp.stages.impl.ci.impl.ecrtodocker
 
 import com.epam.edp.stages.impl.ci.ProjectType
 import com.epam.edp.stages.impl.ci.Stage
+import groovy.json.JsonSlurperClassic
 
 @Stage(name = "ecr-to-docker", buildTool = "any", type = [ProjectType.APPLICATION])
 class EcrToDocker {
     Script script
 
+    def getDockerhubToken (username, accessToken){
+        def dockerhubTokenRaw = script.httpRequest url: "https://hub.docker.com/v2/users/login",
+                            httpMode: "POST",
+                            contentType: 'APPLICATION_JSON',
+                            requestBody: "{\"username\": \"${username}\",\"password\": \"${accessToken}\"}",
+                            consoleLogResponseBody: false,
+                            validResponseCodes: '200'
+        def dockerhubToken = new groovy.json.JsonSlurperClassic().parseText(dockerhubTokenRaw.content).token
+        return dockerhubToken
+    }
+
+    def getDockerhubImageExistanse (repo, token, tag) {
+        def response = script.httpRequest url: "https://hub.docker.com/v2/repositories/${repo}/tags/${tag}",
+                            httpMode: 'GET',
+                            customHeaders: [[name: 'Authorization', value: "Bearer ${token}"]],
+                            consoleLogResponseBody: false,
+                            validResponseCodes: '200,404'
+        return response.status
+    }
+
+    def getAwsRegion(){
+        def awsRegion = script.sh(
+                            script:
+                                "curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | " +
+                                "sed \'s/\\(.*\\)[a-z]/\\1/\'",
+                            returnStdout: true
+                        ).trim()
+        println("[INFO] Current AWS region is ${awsRegion}")
+        return awsRegion
+    }
     void run(context) {
-        def dockerCredentialsId = context.job.getParameterValue(context, "dockerCredentialsId")
-        def repoCredentialsId = context.job.getParameterValue(context, "repoCredentialsId")
-        def edpComponent = "edpcomponent"
-        def dockerRegistry = "docker-registry"
-        def ecr = context.platform.getJsonPathValue(edpComponent, dockerRegistry, ".spec.url")
-        def ecrRepo = context.platform.getJsonPathValue(edpComponent, dockerRegistry, ".metadata.namespace")
+        def ecr = context.platform.getJsonPathValue("edpcomponent", "docker-registry", ".spec.url")
+        def ecrRepo = context.platform.getJsonPathValue("edpcomponent", "docker-registry", ".metadata.namespace")
         def cbImage = context.codebase.config.name
-        def cbTag = context.codebase.buildVersion
 
         script.node("edp-helm") {
-           script.dir("${context.workDir}") {
-              script.withCredentials([script.usernamePassword(credentialsId: "${dockerCredentialsId}",
-                usernameVariable: 'DOCKER_LOGIN', passwordVariable: 'DOCKER_PASS')]) {
+            script.dir("${context.workDir}") {
+                script.openshift.withCluster() {
+                    script.openshift.withProject() {
+                        def awsRegion = getAwsRegion()
+                        def secretName = "dockerhub-credentials"
+                        def secretExist = script.openshift.selector("secrets", secretName).exists()
+
+                        if (!secretExist) {
+                            script.println("[INFO] Codebase ${context.codebase.name} haven't dockerhub secret it will not pushed to Dockerhub \n" +
+                                    "[INFO] To push codebase to DockerHub add secret ")
+                            return
+                        }
+
+                        def secretObject = script.openshift.selector("secrets", secretName).object()
+
+                        byte[] usernameByte = secretObject.data.username.decodeBase64()
+                        byte[] accessTokenByte = secretObject.data.accesstoken.decodeBase64()
+                        byte[] dockerAccountByte = secretObject.data.account.decodeBase64()
+
+                        def dockerAccount = new String(dockerAccountByte)
+                        def dockerhubRepo = "${dockerAccount}/" + "${context.codebase.name}"
+                        def dockerhubUrl = 'docker.io/' + "${dockerhubRepo}"
+                        def accessToken = new String(accessTokenByte)
+                        def username = new String(usernameByte)
+
+                        def dockerhubToken = getDockerhubToken (username, accessToken)
+                        def imageExist = getDockerhubImageExistanse(dockerhubRepo,dockerhubToken,context.codebase.isTag)
+
+                        if (imageExist == 200) {
+                            script.println("[INFO] Image ${dockerhubRepo}:${context.codebase.isTag} already exists in Docker Hub")
+                            return
+                        }
+
+                        script.println("[INFO] Image ${dockerhubRepo}:${context.codebase.isTag} was not found in Docker Hub, start copying")
                         script.sh """
-                            set -ex
-                            awsv2 ecr get-login-password --region eu-central-1 | crane auth login ${ecr} -u AWS --password-stdin
-                            crane auth login index.docker.io -u ${script.DOCKER_LOGIN} -p ${script.DOCKER_PASS}
-                            """
+                                set +x
+                                awsv2 ecr get-login-password --region ${awsRegion} | crane auth login ${ecr} -u AWS --password-stdin
+                                crane auth login index.docker.io -u ${username} -p ${accessToken}
+                                set -ex
+                                crane cp ${ecr}/${ecrRepo}/${cbImage}:${context.codebase.isTag} ${dockerhubUrl}:${context.codebase.isTag}
+                                """
+                    }
                 }
-              script.withCredentials([script.string(credentialsId: "${repoCredentialsId}", variable: 'DH_REPO')]) {
-                        script.sh """
-                            if curl --silent -f --head -lL https://hub.docker.com/v2/repositories/${script.DH_REPO}/${cbImage}/tags/${cbTag}/ > /dev/null; then
-                                echo "${cbImage}:${cbTag} already exists in Docker Hub"
-                                exit 1
-                            else
-                                echo "${cbImage}:${cbTag} was not found in Docker Hub, start copying"
-                                crane cp ${ecr}/${ecrRepo}/${cbImage}:${cbTag} index.docker.io/${script.DH_REPO}/${cbImage}:${cbTag}
-                            fi
-                            """
-                }
-           }
+            }
         }
     }
 }
