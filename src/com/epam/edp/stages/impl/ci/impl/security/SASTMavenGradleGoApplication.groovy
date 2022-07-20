6d129af118d5ab34bc7c7fa2c3a5f6f48b501cb4
Artem Zahumonnyi <Artem_Zahumonnyi@epam.com>
2022-07-20 16:11:35 +0300

    [EPMDEDP-10234]: feat: Add a Static Application Security Testing stage with Semgrep scanner
    Change-Id: I2aaee02111d2e8224676d8bb56dfdd4761725648

---
+++ src/com/epam/edp/stages/impl/ci/impl/security/SASTMavenGradleGoApplication.groovy
@@ -0,0 +1,97 @@
+/* Copyright 2022 EPAM Systems.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+
+package com.epam.edp.stages.impl.ci.impl.security
+
+import org.apache.commons.lang.RandomStringUtils
+import com.epam.edp.stages.impl.ci.ProjectType
+import com.epam.edp.stages.impl.ci.Stage
+import groovy.json.JsonSlurperClassic
+
+@Stage(name = "sast", buildTool = ["maven","gradle","go"], type = [ProjectType.APPLICATION])
+class SASTMavenGradleGoApplication {
+    Script script
+
+    void run(context) {
+        try {
+            script.dir("${context.workDir}") {
+                script.stash name: 'all-repo', includes: "**", useDefaultExcludes: false
+            }
+            def testDir = "${RandomStringUtils.random(10, true, true)}"
+            def defectDojoCredentials = getDefectDojoCredentials(context)
+
+            script.node("sast-edp") {
+                script.dir("${testDir}") {
+                    script.unstash 'all-repo'
+                    def dataFromSemgrepScanner = runSemgrepScanner(context)
+                    publishReport(defectDojoCredentials, dataFromSemgrepScanner)
+                }
+            }
+        }
+        catch (Exception ex) {
+            script.unstable("[JENKINS][WARNING] SAST stage has failed with exception - ${ex}")
+        }
+    }
+
+    def getDefectDojoCredentials(context){
+        def defectDojoSecretJson = context.platform.getJsonValue("secret", "defectdojo-ciuser-token")
+        def parsedDefectDojoSecretJson = new JsonSlurperClassic().parseText(defectDojoSecretJson)
+        def defectDojo = [:]
+        defectDojo.token = new String(parsedDefectDojoSecretJson.data.token.decodeBase64())
+        defectDojo.url = new String(parsedDefectDojoSecretJson.data.url.decodeBase64())
+        return defectDojo
+    }
+
+    def runSemgrepScanner(context) {
+        def edpName = context.platform.getJsonPathValue("cm", "edp-config", ".data.edp_name")
+        def reportData = [:]
+        reportData.active = "true"
+        reportData.verified = "false"
+        reportData.path = "sast-semgrep-report.json"
+        reportData.type = "Semgrep JSON Report"
+        reportData.productTypeName = "Tenant"
+        reportData.productName = "${edpName}"
+        reportData.engagementName = "${context.codebase.name}-${context.git.branch}"
+        reportData.autoCreateContext = "true"
+        reportData.closeOldFindings = "true"
+        reportData.pushToJira = "false"
+        reportData.environment = "Development"
+        reportData.testTitle = "SAST"
+        script.sh (script: """
+                set -ex
+                semgrep --config=auto . --json --output ${reportData.path}
+        """)
+        return reportData
+    }
+
+    def publishReport(credentials, dataReport){
+        script.sh (script: """
+            curl -X POST "${credentials.url}/api/v2/import-scan/" \
+                -H "accept: application/json" \
+                -H "Authorization: Token ${credentials.token}" \
+                -H "Content-Type: multipart/form-data" \
+                -F "scan_date=\$(date +%Y-%m-%d)" \
+                -F "minimum_severity=Info" \
+                -F "active=${dataReport.active}" \
+                -F "verified=${dataReport.verified}" \
+                -F "scan_type=${dataReport.type}" \
+                -F "file=@${dataReport.path};type=application/json" \
+                -F "product_type_name=${dataReport.productTypeName}" \
+                -F "product_name=${dataReport.productName}" \
+                -F "engagement_name=${dataReport.engagementName}" \
+                -F "auto_create_context=${dataReport.autoCreateContext}" \
+                -F "close_old_findings=${dataReport.closeOldFindings}" \
+                -F "push_to_jira=${dataReport.pushToJira}" \
+                -F "environment=${dataReport.environment}" \
+                -F "test_title=${dataReport.testTitle}"
+        """)
+    }
+}
